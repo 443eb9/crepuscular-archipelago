12c7d25fdc566a1115a653418bbdf9c67c988d1e
443eb9#C <443eb9@gmail.com>
2024-06-11 15:16:50 +0800

    Some fix

--- src/components/updates/blog-islands.tsx
+++ src/components/updates/blog-islands.tsx
@@ -2,21 +2,9 @@ import { fetchIslandsMeta } from "@/data/island";
 import IslandCard from "./island-card";
 import { Suspense } from "react";
 import { IslandMeta } from "@/data/model";
-import toast from "react-hot-toast";
-import Toast from "../common/toast";
 
 export default async function BlogIslands({ page, length, tagsFilter }: { page: number, length: number, tagsFilter: number }) {
-    let islands: IslandMeta[] = await fetchIslandsMeta(page * length + 1, length, tagsFilter)
-        .catch((reason) => {
-            const data = reason["response"]["data"];
-            toast.custom(<Toast title="Error" toast={data == undefined ? reason.toString() : data}></Toast>)
-        })
-        .then((value) => {
-            if (value == null) {
-                return;
-            }
-            return value.data;
-        });
+    let islands: IslandMeta[] = (await fetchIslandsMeta(page * length + 1, length, tagsFilter)).data;
 
     return (
         <div className="flex w-full flex-col gap-10">
