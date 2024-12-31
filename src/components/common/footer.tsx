7d3f0efc43404144a04be20f35759988716276f9
443eb9#C <443eb9@gmail.com>
2024-12-31 23:59:58 +0800

    Happy new year!

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <div className="flex flex-col w-full font-bender italic text-light-dark-neutral text-sm">
             <p>
-                Copyright 2024-2024 443eb9#C. All rights reserved.
+                Copyright 2024-2025 443eb9#C. All rights reserved.
             </p>
             <p>
                 Built using <UnderlinedLink href="https://www.rust-lang.org/">Rust</UnderlinedLink> 🦀 and <UnderlinedLink href="https://nextjs.org/">Next.js</UnderlinedLink> ▲, with passion and love.
