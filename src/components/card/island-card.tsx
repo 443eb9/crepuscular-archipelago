4d3a36cd301c30c8039d5b06bff2447b19999faa
443eb9#C <443eb9@gmail.com>
2025-01-23 14:12:51 +0800

    feat: Removed expand button.

--- src/components/card/island-card.tsx
+++ src/components/card/island-card.tsx
@@ -2,31 +2,15 @@
 
 import OutlinedBox from "@/components/outlined-box"
 import { IslandMeta } from "@/data/model"
-import { useEffect, useRef, useState } from "react"
 import CardHeader from "./card-header"
 import clsx from "clsx"
 import CardBody from "./card-body"
 import Text from "@/components/text"
 import CardFooter from "./card-footer"
-import OutlinedButton from "@/components/outlined-button"
 import Link from "next/link"
 import { QueryParams, queryParamsToSearchParams } from "@/data/search-param-util"
 
 export default function IslandCard({ island, content, params, noLink }: { island: IslandMeta, content?: string, params: QueryParams, noLink?: boolean }) {
-    const container = useRef<HTMLDivElement>(null)
-    const [expandState, setExpandState] = useState<boolean | undefined>()
-
-    useEffect(() => {
-        // TODO Image loads slow, but this solution is not elegant
-        setTimeout(() => {
-            if (container.current && container.current.clientHeight > 249 && island.ty == "note") {
-                setExpandState(false)
-            } else {
-                setExpandState(undefined)
-            }
-        }, 1000)
-    }, [island.id])
-
     if (island.isDeleted) {
         return (
             <OutlinedBox className="border-dashed">
@@ -61,23 +45,10 @@ export default function IslandCard({ island, content, params, noLink }: { island
             )}>
                 <div>
                     <CardHeader island={island} />
-                    <div className={clsx("relative overflow-y-clip", { "max-h-[250px]": !expandState })} ref={container}>
+                    <div className="relative overflow-y-clip">
                         {!noLink && <LinkWrapper />}
                         <CardBody island={island} content={content} />
                     </div>
-                    <OutlinedButton
-                        className={clsx(
-                            "mt-2 w-full h-10 border-2 border-light-contrast dark:border-dark-contrast",
-                            { "hidden": expandState == undefined }
-                        )}
-                        onClick={() => setExpandState(!expandState)}
-                    >
-                        {
-                            expandState
-                                ? <Text className="">折叠</Text>
-                                : <Text className="">展开</Text>
-                        }
-                    </OutlinedButton>
                 </div>
                 <CardFooter island={island} params={params} />
             </OutlinedBox>
