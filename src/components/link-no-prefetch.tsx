c86bf26b11de42d154cd9d46e117115ba5f4eeca
443eb9#C <443eb9@gmail.com>
2025-01-24 11:18:54 +0800

    fix: Compile error.

--- src/components/link-no-prefetch.tsx
+++ src/components/link-no-prefetch.tsx
@@ -4,5 +4,5 @@ import React from "react";
 export default function LinkNoPrefetch(props: Omit<React.AnchorHTMLAttributes<HTMLAnchorElement>, keyof LinkProps> & LinkProps & {
     children?: React.ReactNode;
 } & React.RefAttributes<HTMLAnchorElement>) {
-    return <LinkNoPrefetch {...props} prefetch={false} />
+    return <Link {...props} prefetch={false} />
 }
