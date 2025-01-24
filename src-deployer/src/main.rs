2f84b27aac198bf4ce86e327f34d90a3e6dc6045
443eb9#C <443eb9@gmail.com>
2025-01-25 01:23:50 +0800

    feat: Schedule pixiv fetcher.

--- src-deployer/src/main.rs
+++ src-deployer/src/main.rs
@@ -1,8 +1,8 @@
-use clokwerk::TimeUnits;
+use clokwerk::{Interval, TimeUnits};
 use env_logger::Env;
 
 use crate::jobs::{
-    ArtifactFetcher, BackendRunner, ChainedJobs, EventLoop, FrontendRunner, PixivIllustFetcher,
+    ArtifactFetcher, BackendRunner, ChainedJobs, EventLoop, FrontendRunner, Job, PixivIllustFetcher,
 };
 
 mod jobs;
@@ -13,11 +13,7 @@ async fn main() {
     dotenvy::dotenv().unwrap();
     env_logger::init_from_env(Env::default().filter_or("LOG_LEVEL", "info"));
 
-    crate::jobs::Job::run(&mut PixivIllustFetcher::default())
-        .await
-        .unwrap();
-
-    return;
+    let mut pixiv = PixivIllustFetcher::default();
 
     let mut jobs = ChainedJobs::default();
     jobs.push(Box::new(ArtifactFetcher::default()));
@@ -25,6 +21,7 @@ async fn main() {
     jobs.push(Box::new(FrontendRunner::default()));
 
     log::info!("Initial run begin.");
+    let _ = pixiv.wrapped_run().await;
     for job in jobs.iter_mut() {
         let _ = job.wrapped_run().await;
     }
@@ -32,5 +29,6 @@ async fn main() {
 
     EventLoop::new()
         .schedule(jobs, |sc| sc.every(5u32.minutes()))
+        .schedule(pixiv, |sc| sc.every(Interval::Monday))
         .start();
 }
