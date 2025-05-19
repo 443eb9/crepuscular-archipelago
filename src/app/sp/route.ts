5c2247d270ce0bc9502d9c91525ab47efcdebb66
443eb9#C <443eb9@gmail.com>
2025-05-19 12:26:31 +0800

    feat: Special pages.

---
+++ src/app/sp/route.ts
@@ -0,0 +1,16 @@
+import { wrappedFetch } from "@/data/api"
+import { frontendEndpoint } from "@/data/endpoints"
+
+export async function GET(request: Request) {
+    const { search } = new URL(request.url)
+    const target = search.substring(1)
+    const targets = await wrappedFetch<{ [key: string]: number }>(frontendEndpoint("/sp-targets.json"), "GET")
+    if (!targets.ok) {
+        return Response.redirect(frontendEndpoint(""))
+    }
+    const targetIsland = targets.data[target]
+    if (targetIsland == undefined) {
+        return Response.redirect(frontendEndpoint(""))
+    }
+    return Response.redirect(frontendEndpoint(`/islands/${targetIsland}`))
+}
