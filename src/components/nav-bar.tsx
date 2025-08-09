b12ea42efc17b316d45c6fe5cd9fd05111688b7c
443eb9#C <443eb9@gmail.com>
2025-08-09 15:36:16 +0800

    feat: Button animation

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -5,7 +5,7 @@ import TitleText from "./text/title-text";
 export default function NavBar() {
     return (
         <>
-            <OutlinedButton className="w-20 h-10 flex justify-center items-center">
+            <OutlinedButton className="w-20 h-10 flex justify-center items-center" animTo="bottom">
                 <TitleText>
                     <Link href="/updates">动态</Link>
                 </TitleText>
--- src/components/outlined-button.tsx
+++ src/components/outlined-button.tsx
@@ -1,15 +1,39 @@
-import { HTMLAttributes, ReactNode } from "react"
+"use client"
 
-export default function OutlinedButton({ inv, ...props }: { inv?: boolean } & HTMLAttributes<HTMLButtonElement>) {
+import { HTMLAttributes, useState } from "react"
+import * as motion from "motion/react-client";
+import { AnimatePresence } from "motion/react";
+
+export default function OutlinedButton({ inv, animTo, ...props }: { inv?: boolean, animTo?: "top" | "right" | "left" | "bottom" } & HTMLAttributes<HTMLButtonElement>) {
+    const [hover, setHover] = useState(false)
+
+    const _animTo = animTo ?? "top"
+    const horizontal = _animTo == "right" || _animTo == "left"
+
     return (
         <button
             {...props}
             className={
-                `border-2
-                ${inv ? "bg-accent-0 border-accent-0 hover:border-dark-0 hover:dark:border-light-0" : "border-dark-0 dark:border-light-0 hover:bg-accent-0 hover:border-accent-0"}
+                `relative border-2
+                ${inv ? "bg-accent-0 border-accent-0 hover:border-dark-0 hover:dark:border-light-0" : "border-dark-0 dark:border-light-0"}
                 ${props.className}`
             }
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
         >
+            <div className="absolute w-full h-full flex -z-10" style={{ justifyContent: _animTo == "left" ? "end" : "start", alignItems: _animTo == "top" ? "end" : "start" }}>
+                <AnimatePresence>
+                    {
+                        hover &&
+                        <motion.div
+                            initial={{ width: horizontal ? "0%" : "100%", height: horizontal ? "100%" : "0%" }}
+                            animate={{ width: horizontal ? "100%" : "100%", height: horizontal ? "100%" : "100%", transition: { duration: 0.2, ease: "easeOut" } }}
+                            exit={{ width: horizontal ? "0%" : "100%", height: horizontal ? "100%" : "0%", transition: { duration: 0.2, ease: "easeOut" } }}
+                            className="absolute bg-accent-0"
+                        />
+                    }
+                </AnimatePresence>
+            </div>
             {props.children}
         </button>
     )
