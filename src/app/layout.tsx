6c70bd1ef6d45fa258623d28fc6599e718264a76
443eb9#C <443eb9@gmail.com>
2025-08-28 12:19:49 +0800

    fix: Incorrect `lang` attribute.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from "next-themes"
 
 export default function RootLayout({ children }: { children: ReactNode }) {
 	return (
-		<html lang="en" suppressHydrationWarning>
+		<html lang="cmn-hans" suppressHydrationWarning>
 			<body className="bg-light-0 dark:bg-dark-0">
 				<ThemeProvider attribute="class">
 					{children}
